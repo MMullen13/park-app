58b657ad1d975470b658c83063aadfec64559eea
Mark Mullen <mam8022@psu.edu>
2024-10-08 18:36:12 -0400

    Initial Commit

---
+++ WallyLandVacationPlanner/src/model/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
+ */
+package model;
+
+/**
+ * This Model package contains all packages and classes for Logic and Data for the WLVP Application
+ */
---
+++ WallyLandVacationPlanner/src/view/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
+ */
+package view;
+
+/**
+ * This View Package contains all the packages and classes associated with the
+ * user interface. 
+ */
