c9a1349f87f12a2fb2858c65d0d09262d8d2738c
Mark Mullen <mam8022@psu.edu>
2024-10-27 15:33:08 -0400

    Added Classes to OrderView and Confirmation

---
+++ WallyLandVacationPlanner/src/view/foodordering/OrderConfirmationView.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view.foodordering;
+import model.foodordering.Eatery;
+import model.foodordering.Menu;
+import model.foodordering.Order;
+
+/**
+ *
+ * @author theme
+ */
+public class OrderConfirmationView {
+    
+    private Order order;
+
+    public OrderConfirmationView(Order order) {
+        this.order = order;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
